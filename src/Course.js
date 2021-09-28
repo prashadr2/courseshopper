b0a033ac3afb1316fdf6efbe0357faea565c916d
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2021-09-28 16:56:48 -0400

    barebones stuff

---
+++ src/Course.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Course extends React.Component(){
+    constructor(props){
+        super(props);
+        this.state = {crn: 1};
+
+    }
+
+    ComponentDidMount(){
+
+    }
+
+    render(){
+        return(
+
+        );
+    }
+
+}
