17c3901faa6dc809aef8839e18b821f258060b09
Jiarui Ruan <jiaruiruan@plank-160.dynamic2.rpi.edu>
2021-09-29 15:14:25 -0400

    navbar and basic pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+//https://reactrouter.com/web/guides/quick-start
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Pages';
+import Login from './components/Pages/Login';
+import Signup from './components/Pages/Signup'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+      <div> 
+        <input
+          className="input"
+          type="text"
+          placeholder="Search your courses"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar
