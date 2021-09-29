ba23f8ef215e4715a0c7758cc092951cbb2a9fb2
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2021-09-29 16:39:59 -0400

    fixed merge bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import OrcaParse from './OrcaParse'
+import React from 'react';
+import ReactDOM from 'react-dom';
 //https://reactrouter.com/web/guides/quick-start
 import {
   BrowserRouter as Router,
@@ -12,20 +15,41 @@ import Home from './components/Pages';
 import Login from './components/Pages/Login';
 import Signup from './components/Pages/Signup'
 
-function App() {
-  return (
-    <div className="App">
 
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-        </Switch>
-      </Router>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      courses: []
+    }
+  }
+  
+  componentDidMount(){
+    OrcaParse().then(retval=>{
+      // console.log(retval)
+      this.setState({courses: retval});
+    });
+  }
+  
+  render(){
+    return(
+      <div>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </Router>
+      </div>
+      <div className="courselist">
+        {this.state.courses.map(course=>{
+          return(<p>{course.title}</p>);
+        })}
+      </div>
+      </div>
+    );
+  }
 }
-
-export default App;
