8920bd3d5fc799f580f133e50e90d1f6cde8485a
Ryan Prashad <prashadr2@gmail.com>
2021-10-06 16:34:03 -0400

    Revert "Develop"

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import '../App.css'
 
 const SearchBar = () => {
   return (
     <div>
       <div> 
         <input
-          class="form-control" 
-          type="text" 
-          placeholder="Search for courses"
+          className="input"
+          type="text"
+          placeholder="Search your courses"
         />
       </div>
     </div>
