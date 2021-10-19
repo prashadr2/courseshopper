226a4db1df926f8dd75bea6d7afc25dd47e6a301
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2021-10-19 15:28:27 -0400

    tagslicer for store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import courseReducer from './courseSlice'
+import tagReducer from './tagSlice'
 export default configureStore({
   reducer: {
     courses: courseReducer,
+    tags: tagReducer,
   },
 }) 
