238eaa6bbb37103c6c6dd532df2be5c4c4f1f15d
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2021-10-19 18:45:04 -0400

    cleanup

--- src/components/Course/CourseCard.js
+++ src/components/Course/CourseCard.js
@@ -42,23 +42,3 @@ export default class CourseCard extends React.Component {
     )
   }
 }
-
-
-// function CourseCard(props) {
-//   // courseName, courseNumber, courseTitle
-//   return (
-//     <div className="courseCard" style={{ marginTop: "10px" }}>
-//       <div className="title" >
-//         <Link to="/coursepage" style={{ textDecoration: "none", color:"white"}}> 
-//         {props.courseTitle} {props.courseNumber} {props.courseName}
-//         </Link>
-//       </div>
-//       <div style={{ margin: "10px" }}>
-//         <TagList />
-//       </div>
-//       <div className="description">{props.courseDescription}</div>
-//     </div>
-//   )
-// }
-
-// export default CourseCard;
--- src/components/Course/CoursePage.js
+++ src/components/Course/CoursePage.js
@@ -4,7 +4,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { add, remove } from '../../courseSlice'
 import Reviews from './Reviews'
 import Dropzone from 'react-dropzone'
-import {useDropzone} from 'react-dropzone'
 
 const CoursePage = (props) => {
   //required props: courseprefix, coursetitle, coursenumber
@@ -37,7 +36,6 @@ const CoursePage = (props) => {
   }
   const dispatch = useDispatch();
   const clist = useSelector((state) => state.courses.courses);
-  console.log("coursepageprops", props)
   const cid = props.location.state.coursePrefix + '-' + props.location.state.courseNumber;
   let buttontxt;
   if(upload){
@@ -77,70 +75,3 @@ const CoursePage = (props) => {
 }
 
 export default CoursePage;
-
-// export default class CoursePage extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     // console.log(props);
-//     this.state = {
-//         coursename: this.props.location.state.location.state.coursename,
-//         tags: [],
-//         prereqs: "",
-//         termsoffered: "",
-//         description: "",
-//     }
-//   }
-
-//   componentDidMount() {
-    
-//   }
-
-//   professorButton(){
-//     document.getElementById("")
-//   }
-
-//   submitSyllabus(){
-//     let request = {
-//       'pdf': 'axios post test',
-//       'course': 'axios post test',
-//       'professor': 'axios post test'
-//     }
-//     axios.post("https://course-shopper.herokuapp.com/syllabus", request).then(
-//       (response)=>{console.log(response);}, (error)=>{console.log(error);});
-
-//     // axios.get("https://course-shopper.herokuapp.com/syllabus").then(rval=>{console.log(rval);}).catch(error=>{console.log(error);});
-//   }
-
-//   addToCart(){
-//     const dispatch = useDispatch();
-//     dispatch(add())
-//   }
-
-//   render() {
-//     return (
-//       <div className="coursepage">
-//         <div>
-//           <h1>{this.state.coursename}</h1>
-//         </div>
-//         <div>
-//           <p>
-//             Prequisites/Corequisites: {this.state.prereqs} <br/>
-//             When Offered: {this.state.termsoffered}
-//           </p>
-//           <p> {this.state.description}</p>
-//         </div>
-//         <div>
-//           <button onclick="professorButton()">Professors</button>
-//         </div>
-//         <div>
-//           <button onClick={this.submitSyllabus}>Submit Syllabus</button>
-//         </div>
-//         <div>
-//           <button onclick={this.addToCart}>Add To Cart </button>
-//         </div>
-
-
-//       </div>
-//     );
-//   }
-// }
