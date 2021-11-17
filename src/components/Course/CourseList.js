4cb2ad4e4deffb1779bf639975f586c5b53ae131
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2021-11-17 18:06:06 -0500

    navbar reload fix, searchbar implement, comments

--- src/components/Course/CourseList.js
+++ src/components/Course/CourseList.js
@@ -8,22 +8,32 @@ class CourseList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      courses: props.allcourses
+      courses: this.props.allcourses,
+      searchprefix: props.location.state.SearchPrefix
     }
   }
 
   render() {
-    console.log("inlist", this.state.courses);
+    let carray = [];
+    console.log(this.state.searchprefix);
+    this.state.courses.map(course => {
+      if(course.subject_prefix === this.state.searchprefix){
+        carray.push(
+          <CourseCard
+          coursePrefix={course.subject_prefix}
+          courseTitle={course.title}
+          courseNumber={course.number}
+          courseTags={course.tags}
+          />);
+        }
+      }
+    );
+
+    let printval = (carray.length === 0 ? "No results" : carray);
+
     return (
       <div className="courselist">
-        {this.state.courses.map(course => {
-          return <CourseCard
-            coursePrefix={course.subject_prefix}
-            courseTitle={course.title}
-            courseNumber={course.number}
-            courseTags={course.tags}
-            />
-        })}
+        {printval}
       </div>
     );
   }
